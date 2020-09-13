e3ecc147fad169090612c2d381ed6a2c0efbe5f4
he shuai <heshuai@9506@163.com>
2020-09-13 16:09:24 +0800

    rust-project-1-part-1

---
+++ courses/rust/projects/my-porject-1/src/lib.rs
@@ -0,0 +1,23 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        unimplemented!()
+    }
+
+    #[allow(unused_variables)]
+    pub fn set(&mut self, key: String, value: String) {
+        unimplemented!()
+    }
+
+    #[allow(unused_variables)]
+    pub fn get(&self, key: String) -> Option<String> {
+        unimplemented!()
+    }
+
+
+    #[allow(unused_variables)]
+    pub fn remove(&mut self, key: String) {
+        unimplemented!()
+    }
+}
